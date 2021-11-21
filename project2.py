d5a13143be9e79b35fd03c722d3089b4ebde4d62
Fahad <92694740+iamfahad43@users.noreply.github.com>
2021-11-21 10:55:43 -0500

    scrap the data from "realpython.io"

---
+++ project2.py
@@ -0,0 +1,28 @@
+import scrapy
+
+
+class Project2Spider(scrapy.Spider):
+    name = 'project2'
+    allowed_domains = ['www.realpython.github.io']
+    start_urls = ['http://https://realpython.github.io/fake-jobs//']
+
+    def parse(self, response):
+        title = response.css("h1::text").get()
+        head2 = response.css("h2::text").extract()
+        head3 = response.css("h3::text").extract()
+
+        JobTitle = [i +" -> "+ j for i, j in zip(head2, head3)]
+
+        LocData = response.css(".content .location::text").extract()
+
+        PostTime = response.css("time[datatime]::text").extract()
+
+        Location = []
+        for i in LocData:
+            j = i.replace(' ', '')
+            Location.append(j)
+
+        yield
+        {
+                print(title)
+        }
